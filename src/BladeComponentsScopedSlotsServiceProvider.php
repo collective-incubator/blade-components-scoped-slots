8add5d5cc5292740da6957963134643458deb6a6
Hannes Drescher <hannescher9@gmail.com>
2024-02-22 11:11:34 +0100

    feat: improve the code to not require brackets around a single function parameter and allow nesting

--- src/BladeComponentsScopedSlotsServiceProvider.php
+++ src/BladeComponentsScopedSlotsServiceProvider.php
@@ -4,6 +4,7 @@ namespace KonradKalemba\BladeComponentsScopedSlots;
 
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\HtmlString;
 
 class BladeComponentsScopedSlotsServiceProvider extends ServiceProvider
 {
@@ -25,21 +26,25 @@ class BladeComponentsScopedSlotsServiceProvider extends ServiceProvider
             // Extract values from the directive's arguments array
             [$name, $functionArguments, $functionUses] = $directiveArguments;
 
+            // Wrap function arguments in parentheses if they don't already have them
+            if ($functionArguments && !preg_match('/^\(.*\)$/', $functionArguments))
+                $functionArguments = "({$functionArguments})";
+
             // Connect the arguments to form a correct function declaration
             if ($functionArguments) $functionArguments = "function {$functionArguments}";
             
             $functionUses = array_filter(explode(',', trim($functionUses, '()')), 'strlen');
             
             // Add `$__env` to allow usage of other Blade directives inside the scoped slot
-            array_push($functionUses, '$__env');
+            $functionUses[] = '$__env';
 
             $functionUses = implode(',', $functionUses);
 
-            return "<?php \$__env->slot({$name}, {$functionArguments} use ({$functionUses}) { ?>"; 
+            return "<?php \$__env->slot({$name}, {$functionArguments} use ({$functionUses}) { ob_start(); ?>";
         });
 
         Blade::directive('endscopedslot', function () {
-            return "<?php }); ?>";
+            return "<?php return new HtmlString(trim(ob_get_clean())); }); ?>";
         });
     }
 }
